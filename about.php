8465b0ae07fa1384391c98a54b2abbf9d22015d4
maihuuphuc <maihuuphuc2k@gmail.com>
2023-11-11 23:31:10 +0700

    khoi tao giao dien danh chinh cua he thong giao dien admin phat trien sau

---
+++ about.php
@@ -0,0 +1,45 @@
+<?php
+  include "ShareView/header.php";
+
+?>
+
+
+</div>
+
+  <!-- about section -->
+
+  <section class="about_section layout_padding">
+    <div class="container  ">
+
+      <div class="row">
+        <div class="col-md-6 ">
+          <div class="img-box">
+            <img src="images/about-img.png" alt="">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="detail-box">
+            <div class="heading_container">
+              <h2>
+                tiêu đề về chúng tôi 
+              </h2>
+            </div>
+            <p>
+             thông tin về chúng tôi ....
+            </p>
+            <a href="">
+              Đọc thêm
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end about section -->
+
+  <!-- footer section -->
+<?php 
+    include "ShareView/footer.php";
+
+?>
